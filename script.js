b2b30b1f7675c1a962745ff5e2589494bc385fbc
Huncho7 <90630690+Huncho7@users.noreply.github.com>
2023-02-11 09:39:22 +0100

    More functionalities

--- script.js
+++ script.js
@@ -27,3 +27,29 @@ document.querySelector(".square").addEventListener("click", function () {
   let input2 = Number(document.querySelector(".input2").value);
   document.querySelector(".answer").value = input1 ** input2;
 });
+
+const main = document.querySelector('main');
+const overlay = document.querySelector('.overlay')
+
+const addHidden = function(){
+  main.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+const removeHidden = function(){
+  main.classList.remove("hidden");
+  overlay.classList.remove("hidden")
+  
+}
+
+document.querySelector('.open').addEventListener('click', removeHidden);
+document.querySelector('.close').addEventListener('click', addHidden);
+
+document.addEventListener('keydown', (event) => {
+  if(event.key === 'Escape' && !main.classList.contains("hidden")){
+    main.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
+})
+
+
